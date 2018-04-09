526cb03fcc52c78e110f0d46332771e14898c18f
Pablo Ovejero <povejero@eventbrite.com>
2018-04-09 01:58:20 -0300

    modify migration

--- GE/migrations/0004_auto_20170516_0501.py
+++ GE/migrations/0004_auto_20170516_0501.py
@@ -31,7 +31,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='attentiontype',
             name='id_attention_type',
-            field=models.AutoField(default=0, primary_key=True, serialize=False),
+            field=models.AutoField(primary_key=True, serialize=False),
             preserve_default=False,
         ),
         migrations.AddField(
